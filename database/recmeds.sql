a15a7f68257f8fb462c7917a6053b4fae16a818d
Sudhir Daga <41622136+sudzzz@users.noreply.github.com>
2020-06-13 11:05:47 +0530

    Update recmeds.sql

--- database/recmeds.sql
+++ database/recmeds.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 12, 2020 at 05:37 PM
+-- Generation Time: Jun 13, 2020 at 07:17 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.31
 
@@ -35,14 +35,21 @@ CREATE TABLE `coustomer` (
   `lng` float(10,6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `coustomer`
+-- Table structure for table `expire`
 --
 
-INSERT INTO `coustomer` (`id`, `username`, `address`, `lat`, `lng`) VALUES
-(2, 'Sudhir18', 'Buddha Park\r\nB 14, Block B, Kalyani, West Bengal 741235\r\n', 22.964602, 88.428726),
-(3, 'Sanjay', 'Central Park,\r\nB5, Block B, Kalyani, West Bengal 741235', 22.974878, 88.434555),
-(5, 'raj', 'Bangur Park Rishra', 22.721649, 88.350647);
+CREATE TABLE `expire` (
+  `id` int(255) NOT NULL,
+  `medicine_name` varchar(50) NOT NULL,
+  `company_name` varchar(50) NOT NULL,
+  `number_tablets` int(100) NOT NULL,
+  `store_name` varchar(50) NOT NULL,
+  `store_address` varchar(150) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -57,13 +64,6 @@ CREATE TABLE `hospital` (
   `lat` float(10,6) DEFAULT NULL,
   `lng` float(10,6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `hospital`
---
-
-INSERT INTO `hospital` (`id`, `username`, `address`, `lat`, `lng`) VALUES
-(1, 'JNM', 'Kolkata JNM Hospital quarter E/88, Kalyani, West Bengal 741235', 22.974371, 88.455711);
 
 -- --------------------------------------------------------
 
@@ -81,19 +81,6 @@ CREATE TABLE `orders` (
   `dod` date NOT NULL,
   `status` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`id`, `hospital`, `hospital_address`, `store`, `medicine_name`, `number_tablets`, `dod`, `status`) VALUES
-(11, 'JNM Hospital', 'Kolkata JNM Hospital quarter E/88, Kalyani, West Bengal 741235', 'Mishra Medical Stores', 'Calpol-Paracetamol', 20, '2020-06-14', 1),
-(12, 'JNM Hospital', 'Kolkata JNM Hospital quarter E/88, Kalyani, West Bengal 741235', 'Daga Medical Store', 'Calpol-Paracetamol', 10, '2020-06-23', 1),
-(13, 'JNM Hospital', 'Kolkata JNM Hospital quarter E/88, Kalyani, West Bengal 741235', 'Mishra Medical Stores', 'Pan 40-Dabur', 20, '2020-06-14', 1),
-(14, 'JNM Hospital', 'Kolkata JNM Hospital quarter E/88, Kalyani, West Bengal 741235', 'United Medicine Store', 'Calpol-Paracetamol', 15, '2020-06-14', 0),
-(15, 'JNM Hospital', 'Kolkata JNM Hospital quarter E/88, Kalyani, West Bengal 741235', 'Mishra Medical Stores', 'Calpol-Paracetamol', 15, '2020-06-14', 0),
-(16, 'JNM Hospital', 'Kolkata JNM Hospital quarter E/88, Kalyani, West Bengal 741235', 'Yamaha Medicals', 'Pan 40-Dabur', 20, '2020-06-15', 1),
-(17, 'JNM Hospital', 'Kolkata JNM Hospital quarter E/88, Kalyani, West Bengal 741235', 'Divya Daga', 'paracetamol 650-Cipla', 10, '2020-06-14', 1);
 
 -- --------------------------------------------------------
 
@@ -113,19 +100,6 @@ CREATE TABLE `pharmacy` (
   `lng` float(10,6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `pharmacy`
---
-
-INSERT INTO `pharmacy` (`id`, `username`, `name`, `medicine_name`, `number_tablets`, `company_name`, `expiry_date`, `lat`, `lng`) VALUES
-(25, 'Gita', 'Gita Medicine Corner', 'Calpol', 55, 'Paracetamol', '2020-06-14', 22.962732, 88.428391),
-(27, 'United', 'United Medicine Store', 'Calpol', 15, 'Paracetamol', '2020-08-13', 22.966860, 88.467033),
-(32, 'Daga', 'Daga Medical Store', 'Calpol', 35, 'Paracetamol', '2020-06-22', 22.970528, 88.444626),
-(33, 'uddeshya18', 'Mishra Medical Stores', 'Calpol', 25, 'Paracetamol', '2020-06-25', 22.971842, 88.428001),
-(34, 'Yamaha', 'Yamaha Medicals', 'Pan 40', 25, 'Dabur', '2020-08-29', 22.980160, 88.437988),
-(35, 'Daga', 'Daga Medical Store', 'Pan 40', 25, 'Dabur', '2020-06-11', 22.970528, 88.444626),
-(36, 'uddeshya18', 'Mishra Medical Stores', 'Pan 40', 50, 'Dabur', '2020-06-09', 22.971842, 88.428001);
-
 -- --------------------------------------------------------
 
 --
@@ -143,39 +117,6 @@ CREATE TABLE `register` (
   `usertype` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `register`
---
-
-INSERT INTO `register` (`id`, `name`, `address`, `phone`, `username`, `email`, `password`, `usertype`) VALUES
-(2, 'Mishra Medical Stores', 'B-14/5  Kalyani, West Bengal - 741235 ', '7552145338', 'uddeshya18', 'uddeshya1812@gmail.com', '123456', 'pharmacy'),
-(3, 'Sevak Sang', 'B-9/11(CA), Kalyani, B 9, Block B, Kalyani, West Bengal 741235', '8240078072', 'Satya', 'singhsatyaprakash19@gmail.com', 'satya123', 'hospital'),
-(24, 'Daga Medical Store', 'Lake Park\r\nBlock B, Kalyani, West Bengal 741235\r\n', '8335940590', 'Daga', 'sudhirdaga1998@gmail.com', '12345', 'pharmacy'),
-(29, 'Singh Medical Store', 'B-14/12, Kalyani, West Bengal - 741235 ', '8240078072', 'Satya18', 'satya@gmail.com', '12345', 'pharmacy'),
-(31, 'Sudhir Daga', 'Buddha Park\r\nB 14, Block B, Kalyani, West Bengal 741235\r\n', '8335940590', 'Sudhir18', 'sudhirdaga18@gmail.com', '12345', 'customer'),
-(32, 'Sanjay Tiwari', 'B5, Block B, Kalyani, West Bengal 741235', '8209570373', 'Sanjay', 'sanjay981127@gmail.com', '12345', 'customer'),
-(34, 'United Medicine Store', 'Bidhanpally, Kalyani, West Bengal 741235', '1234567890', 'United', 'unitedmedicine@gmail.com', '12345', 'pharmacy'),
-(35, 'Gita Medicine Corner', 'B-17/176, near I.T.I More, Kalyani, West Bengal 741235', '0123456789', 'Gita', 'gitamedicine@gmail.com', '12345', 'pharmacy'),
-(36, 'Baishakhi Medical Stores', 'main station road, A 9X, Block A9, Q4(S, Kalyani, West Bengal 741235', '0123456789', 'Baishakhi', 'baishakhimedicine@gmail.com', '12345', 'pharmacy'),
-(37, 'Zenith Medical Stores', 'B-9/274, Kalyani, Kolkata, West Bengal', '0123456789', 'Zenith', 'zenithmedicine@gmail.com', '12345', 'pharmacy'),
-(38, 'Medical Pharmacy', 'Kalyani, Block A11, Block A, Kalyani, West Bengal 741235', '0123456789', 'Medical', 'medicalmedicine@gmail.com', '12345', 'pharmacy'),
-(39, 'Frankross Pharmacy Kalyani Central Park', 'HOLDING NO.B-12/17(S),PLOT NO.17(S SUBLOCK-B, Kalyani, West Bengal 741235', '0123456789', 'Frankross', 'frankrossmedicine@gmail.com', '12345', 'pharmacy'),
-(40, 'Swadesh Medical Hall', 'B-9/20 (C.A), Kalyani, West Bengal', '0123456789', 'Swadesh', 'swadeshmedicine@gmail.com', '12345', 'pharmacy'),
-(41, 'Dev medical', 'B-18, 199, Block B, kalyanii, Kalyani, West Bengal 741235', '0123456789', 'Dev', 'devmedicine@gmail.com', '12345', 'pharmacy'),
-(42, 'Samriddha Pharmacy', 'Kalyani Sangam cinema Hall building, Kalyani, West Bengal 741235', '0123456789', 'Samriddha', 'samriddhamedicine@gmail.com ', '12345', 'pharmacy'),
-(43, 'Prarthana Medical', 'D.C. Building, A-9/156, Kalyani, Beside SNR Carnival, Opp, A1, Block A1, Block A, Kolkata, West Bengal 741235', '0123456789', 'Prarthana', 'prarthanamedicine@gmail.com', '12345', 'pharmacy'),
-(45, 'Frank Ross Pharmacy - Kalyani', 'A-2/52, JNM Hospital Rd, opposite Bidhan Park, Block A2, Block A, Kalyani, West Bengal 741235', '0123456789', 'Frank', 'frankross@gmail.com', '12345', 'pharmacy'),
-(46, 'Chakrabarty Medical', 'B-11/7, Near Kalyani Poursabha, Kalyani, West Bengal 741235', '0123456789', 'Chakrabarty', 'chakrabartymedicine@gmail.com', '12345', 'pharmacy'),
-(47, 'Care Pharmacy', ' B 7/28 South, Central Park, 2 No Bazar Road, Kalyani, West Bengal 741235', '0123456789', 'Care', 'caremedicine@gmail.com', '12345', 'pharmacy'),
-(48, 'SastaSundar-Kalyani', ' B 7/8 s, Central Park, behind HDFC & Canara Bank Healthbuddy Kalyani Pharmacy, Kalyani, West Bengal 741235', '0123456789', 'SastaSundar', 'sastasundar@gmail.com', '12345', 'pharmacy'),
-(49, 'Saha Medical', 'A9/15 (s) kalyani Nadia, 741235', '0123456789', 'Saha', 'sahamedicine@gmail.com', '12345', 'pharmacy'),
-(50, 'Nehan Medical Shop', 'B7, Block B, Kanchrapara, West Bengal 741235', '0123456789', 'Nehan', 'nehanmedicine@gmail.com', '12345', 'pharmacy'),
-(51, 'Dey Medico', '2 No Bazar, B-7/29S), Central Park, Namita Rd, Block B, Kalyani, West Bengal 741235', '0123456789', 'Dey', 'deymedicine@gmail.com', '12345', 'pharmacy'),
-(52, 'Yamaha Medicals', 'Block B-9/4(CA), Kalyani (ITI More, West Bengal 741235', '0123456789', 'Yamaha', 'yamahamedicine@gmail.com', '12345', 'pharmacy'),
-(53, 'JNM Hospital', 'Kolkata JNM Hospital quarter E/88, Kalyani, West Bengal 741235', '0123456789', 'JNM', 'jnmhospital@gmail.com', '12345', 'hospital'),
-(55, 'Divya Daga', '179, Bangur Park,Rishra,Hooghly', '9681287066', 'divya028', 'daga.divya028@gmail.com', '123456', 'pharmacy'),
-(56, 'Raj', 'Bangur Park Rishra', '6666222245', 'raj', 'raj@gmail.com', '12345', 'customer');
-
 -- --------------------------------------------------------
 
 --
@@ -192,34 +133,6 @@ CREATE TABLE `stores` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `stores`
---
-
-INSERT INTO `stores` (`id`, `username`, `name`, `address`, `lat`, `lng`) VALUES
-(1, 'United', 'United Medicine Store', 'Bidhanpally, Kalyani, West Bengal 741235', 22.966860, 88.467033),
-(2, 'Gita', 'Gita Medicine Corner', 'B-17/176, near I.T.I More, Kalyani, West Bengal 741235', 22.962732, 88.428391),
-(3, 'Baishakhi', 'Baishakhi Medical Stores', 'main station road, A 9X, Block A9, Q4(S, Kalyani, West Bengal 741235', 22.969345, 88.466644),
-(4, 'Zenith', 'Zenith Medical Stores', 'B-9/274, Kalyani, Kolkata, West Bengal', 22.975954, 88.437210),
-(5, 'Medical', 'Medical Pharmacy', 'Kalyani, Block A11, Block A, Kalyani, West Bengal 741235', 22.965101, 88.454460),
-(6, 'Frankross', 'Frankross Pharmacy Kalyani Central Park', 'HOLDING NO.B-12/17(S),PLOT NO.17(S SUBLOCK-B, Kalyani, West Bengal 741235', 22.966608, 88.436752),
-(7, 'Swadesh', 'Swadesh Medical Hall', 'B-9/20 (C.A), Kalyani, West Bengal', 22.977104, 88.436142),
-(8, 'Dev', 'Dev medical', 'B-18, 199, Block B, kalyanii, Kalyani, West Bengal 741235', 22.986689, 88.429398),
-(9, 'Samriddha', 'Samriddha Pharmacy', 'Kalyani Sangam cinema Hall building, Kalyani, West Bengal 741235', 22.966608, 88.436752),
-(10, 'Prarthana', 'Prarthana Medical', 'D.C. Building, A-9/156, Kalyani, Beside SNR Carnival, Opp, A1, Block A1, Block A, Kolkata, West Bengal 741235', 22.969088, 88.465195),
-(11, 'Frank', 'Frank Ross Pharmacy - Kalyani', 'A-2/52, JNM Hospital Rd, opposite Bidhan Park, Block A2, Block A, Kalyani, West Bengal 741235', 22.971575, 88.462654),
-(12, 'Chakrabarty', 'Chakrabarty Medical', 'B-11/7, Near Kalyani Poursabha, Kalyani, West Bengal 741235', 22.977390, 88.445717),
-(13, 'Care', 'Care Pharmacy', ' B 7/28 South, Central Park, 2 No Bazar Road, Kalyani, West Bengal 741235', 22.976109, 88.434143),
-(14, 'SastaSundar', 'SastaSundar-Kalyani', ' B 7/8 s, Central Park, behind HDFC & Canara Bank Healthbuddy Kalyani Pharmacy, Kalyani, West Bengal 741235', 22.976080, 88.433319),
-(15, 'Saha', 'Saha Medical', 'A9/15 (s) kalyani Nadia, 741235', 22.970520, 88.464500),
-(16, 'Nehan', 'Nehan Medical Shop', 'B7, Block B, Kanchrapara, West Bengal 741235', 22.975611, 88.433853),
-(17, 'Dey', 'Dey Medico', '2 No Bazar, B-7/29S), Central Park, Namita Rd, Block B, Kalyani, West Bengal 741235', 22.976120, 88.434135),
-(18, 'Satya18', 'Singh Medical Store', 'B-14/12, Kalyani, West Bengal - 741235 ', 22.965216, 88.434334),
-(19, 'uddeshya18', 'Mishra Medical Stores', 'B-4/5(S)  Kalyani, West Bengal - 741235 ', 22.971842, 88.428001),
-(21, 'Daga', 'Daga Medical Store', 'Lake Park\r\nBlock B, Kalyani, West Bengal 741235\r\n', 22.970528, 88.444626),
-(22, 'Yamaha', 'Yamaha Medicals', 'Block B-9/4(CA), Kalyani (ITI More, West Bengal 741235', 22.980160, 88.437988),
-(23, 'divya028', 'Divya Daga', '179, Bangur Park,Rishra,Hooghly', 22.720577, 88.346375);
-
---
 -- Indexes for dumped tables
 --
 
@@ -229,6 +142,12 @@ INSERT INTO `stores` (`id`, `username`, `name`, `address`, `lat`, `lng`) VALUES
 ALTER TABLE `coustomer`
   ADD PRIMARY KEY (`id`),
   ADD KEY `coustomer_fk` (`username`);
+
+--
+-- Indexes for table `expire`
+--
+ALTER TABLE `expire`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `hospital`
@@ -275,37 +194,43 @@ ALTER TABLE `stores`
 -- AUTO_INCREMENT for table `coustomer`
 --
 ALTER TABLE `coustomer`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `expire`
+--
+ALTER TABLE `expire`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `hospital`
 --
 ALTER TABLE `hospital`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `pharmacy`
 --
 ALTER TABLE `pharmacy`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `register`
 --
 ALTER TABLE `register`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `stores`
 --
 ALTER TABLE `stores`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables

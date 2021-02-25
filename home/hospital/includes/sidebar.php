81d214de57e2392fae84a6b19995b1775a5b42f2
sudzzz <sudhirdaga1998@gmail.com>
2021-02-25 14:56:07 +0530

    project

--- home/hospital/includes/sidebar.php
+++ home/hospital/includes/sidebar.php
@@ -1,11 +1,29 @@
 <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-    <div class="sidebar-sticky">
+    <div id = "sidebar" class="sidebar-sticky">
         <ul class="nav flex-column">
             <li class="nav-item">
-                <a class="nav-link" href="order.php">
-                    <span data-feather="clipboard"></span>
-                    View Orders
-                </a>
+              <a class="nav-link" href="bloodRequest.php">
+                  <span data-feather="clipboard"></span>
+                  Blood Request
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="bloodDeliver.php">
+                  <span data-feather="clipboard"></span>
+                  Blood Delivery
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="medicineRequest.php">
+                  <span data-feather="clipboard"></span>
+                  Meds Request
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="medicineDeliver.php">
+                  <span data-feather="clipboard"></span>
+                  Meds Delivery
+              </a>
             </li>
         </ul>
     </div>

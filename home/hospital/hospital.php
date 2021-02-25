dea41c77af34e77ff7b87da61923dc2771f01542
sudzzz <sudhirdaga1998@gmail.com>
2021-02-25 14:58:04 +0530

    project

--- home/hospital/hospital.php
+++ home/hospital/hospital.php
@@ -3,18 +3,78 @@ SESSION_START();
 
 $mysqli = new mysqli("localhost","root","","recmeds") or die(mysqli_error($mysqli));
 
+$address = $_SESSION['address'];
+$url = "https://maps.google.com/maps/api/geocode/json?address=".urlencode($address)."&key=AIzaSyB1H3RZBpWVgwGNrtxiI1rCjFrLaM4LweI";
 
-if(!isset($_SESSION['username']))
-{
-  header('location: ../../index.php');
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$responseJson = curl_exec($ch);
+curl_close($ch);
+
+$response = json_decode($responseJson);
+
+if ($response->status == 'OK') {
+  $latitude = $response->results[0]->geometry->location->lat;
+  $longitude = $response->results[0]->geometry->location->lng;
+
+  $mysqli->query("UPDATE hospital SET lat='$latitude',lng='$longitude' WHERE username='{$_SESSION['username']}'") or die($mysqli->error);
+} else {
+  echo $response->status;
+  var_dump($response);
 }
 
 $searchKey = "";
+$user = $_SESSION['username'];
+$hospital = $_SESSION['name'];
+$words = str_word_count($hospital,1);
+$hos = strtolower($words[0]);
+$result = $mysqli->query("SELECT lat,lng FROM hospital WHERE username='$user'");
+$row = $result->fetch_assoc();
+$lat = $row['lat'];
+$lng = $row['lng'];
 
-$date1 = date("Y-m-d");
-$date = strtotime($date1);
+$API_KEY = 'AIzaSyB1H3RZBpWVgwGNrtxiI1rCjFrLaM4LweI';
 
+$request = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+
+$params  = array(
+  "location" => $lat.",".$lng,
+  "radius" => 8000,
+  "type" => "hospital",
+  "key"   => $API_KEY,
+);
+
+$request .= http_build_query($params);
+
+$json = file_get_contents($request);
+$data = json_decode($json, true);
+$array = $data['results'];
+foreach ($array as $arr) {
+  // code...
+  $name = $arr['name'];
+  $name = strtolower($name);
+  $latitude =  $arr['geometry']['location']['lat'];
+  $longitude = $arr['geometry']['location']['lng'];
+  $address = $arr['vicinity'];
+  if(strpos($name, $hos) !== false)
+  {
+      continue;
+  }
+  else {
+    $name = ucwords($name);
+    $res =  $mysqli->query("SELECT * FROM nearby WHERE name='$name'");
+    $r = $res->fetch_assoc();
+    if(empty($r)){
+      $mysqli->query("INSERT INTO nearby(name,address,lat,lng) VALUES('$name','$address','$latitude','$longitude')") or die($mysqli->error);
+    }
+    else {
+      break;
+    }
+  }
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -25,86 +85,57 @@ $date = strtotime($date1);
   <body>
     <!-- Header banner -->
     <?php require_once 'includes/header.php'; ?>
-
-    <div  class="container-fluid">
-      <?php
-      //To get latitude and longitude of the hospital.
-      $address = $_SESSION['address'];
-      $url = "https://maps.google.com/maps/api/geocode/json?address=".urlencode($address)."&key=AIzaSyB1H3RZBpWVgwGNrtxiI1rCjFrLaM4LweI";
-
-      $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL, $url);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      $responseJson = curl_exec($ch);
-      curl_close($ch);
-
-      $response = json_decode($responseJson);
-
-      if ($response->status == 'OK') {
-        $latitude = $response->results[0]->geometry->location->lat;
-        $longitude = $response->results[0]->geometry->location->lng;
-
-        $mysqli->query("UPDATE hospital SET lat='$latitude',lng='$longitude' WHERE username='{$_SESSION['username']}'") or die($mysqli->error);
-      } else {
-        echo $response->status;
-        var_dump($response);
-      }
-       ?>
-       <div class="row">
-           <!-- Sidebar menu -->
-           <?php require_once 'includes/sidebar.php'; ?>
-           <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-               <h1 style="margin-top: 10px">Medicine Records</h1>
-               <?php
-                $result = $mysqli->query("SELECT * from pharmacy");
-                if(isset($_POST['search']))
-                {
-                  $searchKey = $_POST['search'];
-                  $result = $mysqli->query("SELECT * from pharmacy WHERE medicine_name LIKE '%$searchKey%'");
-                }
-               ?>
-               <!-- Search form -->
-               <form action="" method="POST">
-    					    <div class="col-md-6">
-    						  <input type="text" name="search" class='form-control' placeholder="Search By Medicine Name" value="<?php echo $searchKey; ?>" >
-                  <button class="btn btn-info justify-content-center" style="margin-top:10px; margin-bottom:10px;">Search</button>
-                  </div>
-      				</form>
-               <div class="row justify-content-center">
-                 <table class = "table">
-                   <thead>
-                     <tr>
-                       <th>Store Name</th>
-                       <th>Medicine Name</th>
-                       <th>No. Of Tablets</th>
-                       <th>Expiry Date</th>
-                       <th>Order</th>
-                     </tr>
-                   </thead>
-                   <?php while ($row = $result->fetch_assoc()): ?>
-                     <tr>
-                       <?php if((strtotime($row['expiry_date'])-$date)>0): ?>
-                       <td><?php echo $row['name']; ?></td>
-                       <td><?php echo $row['medicine_name'].'-'.$row['company_name']; ?></td>
-                       <td><?php echo $row['number_tablets']; ?></td>
-                       <td><?php echo $row['expiry_date']; ?></td>
-                       <td>
-                          <a href="checkout.php?order=<?php echo $row['id']; ?>"
-                           class = "btn btn-info" name="order">Order Now</a>
-                       </td>
-                       <?php endif; ?>
-                     </tr>
-                   <?php endwhile; ?>
-                 </table>
-               </div>
-           </main>
-       </div>
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Sidebar menu -->
+            <?php require_once 'includes/sidebar.php'; ?>
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+              <h1 style="margin-top: 10px">Nearby Hospitals</h1>
+              <?php
+               $result = $mysqli->query("SELECT * from nearby");
+               if(isset($_POST['search']))
+               {
+                 $searchKey = $_POST['search'];
+                 $result = $mysqli->query("SELECT * from nearby WHERE name LIKE '%$searchKey%'");
+               }
+              ?>
+              <!-- Search form -->
+              <form action="" method="POST">
+                 <div class="col-md-6">
+                 <input type="text" name="search" class='form-control' placeholder="Search By Hospital Name" value="<?php echo $searchKey; ?>" >
+                 <button class="btn btn-info justify-content-center" style="margin-top:10px; margin-bottom:10px;">Search</button>
+                 </div>
+             </form>
+             <div class="row justify-content-center">
+               <table class = "table">
+                 <thead>
+                   <tr>
+                     <th>Hospital Name</th>
+                     <th>Address</th>
+                     <th>Request Blood</th>
+                     <th>Request Medicine</th>
+                   </tr>
+                 </thead>
+                 <?php while ($row = $result->fetch_assoc()): ?>
+                   <tr>
+                     <td><?php echo $row['name']; ?></td>
+                     <td><?php echo $row['address']; ?></td>
+                     <td>
+                        <a href="blood.php?request=<?php echo $row['id']; ?>"
+                         class = "btn btn-info" name="blood">Request Blood</a>
+                     </td>
+                     <td>
+                        <a href="medicine.php?request=<?php echo $row['id']; ?>"
+                         class = "btn btn-info" name="medicine">Request Meds</a>
+                     </td>
+                   </tr>
+                 <?php endwhile; ?>
+               </table>
+             </div>
+            </main>
+        </div>
     </div>
-
     <!-- Footer scripts, and functions -->
     <?php require_once 'includes/footer.php'; ?>
   </body>
-  <script async defer
-    src="https://maps.googleapis.com/maps/api/js?&libraries=places&key=AIzaSyB1H3RZBpWVgwGNrtxiI1rCjFrLaM4LweI&callback=loadMap">
-  </script>
 </html>
